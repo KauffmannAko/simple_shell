69607545a8609b4c4c7c8025b6411ddbf952497a
KauffmannAko <nanakauffmann@gmail.com>
2022-08-26 05:59:00 +0100

    implement Display a prompt and wait for the user to type a command and added main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <signal.h>
+char *read_line(void);
+#endif
