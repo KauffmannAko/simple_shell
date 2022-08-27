f049b381eafb775c1fd3dd004c6179c81e75e00a
KauffmannAko <nanakauffmann@gmail.com>
2022-08-27 14:28:46 +0100

    Handle command lines with arguments ,Handle the PATH

--- main.h
+++ main.h
@@ -10,4 +10,10 @@
 #include <limits.h>
 #include <signal.h>
 char *read_line(void);
+void _puts(char *str);
+int _putchar(char c);
+unsigned int _strlen(char *s);
+char **find_path(char **environ);
+char *_getenv(char **environ, char *dirname);
+char *args_path(char **parse, char **new);
 #endif
